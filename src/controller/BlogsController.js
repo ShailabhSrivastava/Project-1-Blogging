63d4f90de276ce0e7443b972f4e59ee26d5f0315
Shailabh <shaisri681@gmail.com>
2022-09-05 20:53:48 +0530

    Iam

--- src/controller/BlogsController.js
+++ src/controller/BlogsController.js
@@ -4,6 +4,10 @@ const authorModel = require("../Model/authorModel")
 const createBlogs = async function (req, res) {
     try {
         let data = req.body
+        let author= await authorModel.findById(data.author_id)
+        if (!author){
+            res.status(404).send({msg: "author ID is not valid", status:false})
+        } 
         let savedData = await BlogModel.create(data)
         return res.status(201).send({ msg: savedData })
     }
