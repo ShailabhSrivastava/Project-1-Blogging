530128ab672153b9a428f7ff5e890e96e63af807
Shailabh <shaisri681@gmail.com>
2022-09-05 16:51:46 +0530

    Iam

---
+++ src/Model/authorModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+    
+
+    },
+    title: {
+        type: String,
+        required: true,
+        enum: ["Mr", "Mrs", "Miss"]
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true });
+
+
+
+module.exports = mongoose.model('authorProject', authorSchema)
