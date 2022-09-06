837048b21d0633a7fdac9a667c5da16ea115d4d1
Dharm-8434 <dharm15121998@gmail.com>
2022-09-06 21:04:06 +0530

    done by dharmpal

--- src/controller/BlogsController.js
+++ src/controller/BlogsController.js
@@ -18,6 +18,67 @@ const createBlogs = async function (req, res) {
     }
 }
 
+let getBlogs = async function (req, res) {
+
+    try {
+
+        let filter = { isDeleted: false, isPublished: true }
+        let queryValue = req.query
+
+        //======================================= Start Validation===============================================//
+
+        if (queryValue["author Id"]) {
+
+            if (typeof queryValue["author Id"] !== "string" || typeof queryValue["author Id"] == "undefined") {
+                return res.status(400).send({ msg: "authorId is required", status: false })
+            }
+            if (queryValue["author Id"].length == 0) {
+                return res.status(400).send({ msg: "authorId must be present", status: false })
+            }
+            if (!mongoose.Types.ObjectId.isValid(queryValue["author Id"])) {
+                return res.status(400).send({ msg: "authorId is is invalid", status: false })
+            }
+
+            let author = await authorModel.findById(queryValue["author Id"])
+
+            if (!author) {
+                return res.status(404).send({ msg: "athorId is not from author collection", status: false })
+            }
+
+            filter["authorId"] = queryValue["author Id"]
+        }
+        if (queryValue["category"]) {
+
+            if (typeof queryValue["category"] !== "string" || typeof queryValue["category"] == "undefined") {
+                return res.status(400).send({ msg: "category is required", status: false })
+            }
+
+            filter["category"] = queryValue["category"]
+        }
+        if (queryValue["tags"]) {
+
+            filter["tags"] = queryValue["tags"]
+        }
+        if (queryValue["subscategory"]) {
+
+            filter["subscategory"] = queryValue["subscategory"]
+        }
+
+        let data = await BlogModel.find(filter)
+
+        if (data.length == 0) {
+
+            return res.status(404).send({ status: false, msg: "No document found" })
+        }
+
+        res.status(200).send({ data: data })
+
+    } catch (err) {
+
+        res.status(500).send({ status: false, msg: err.message })
+    }
+}
+
 
 const updateBlog = async function (req, res) {
     try {
@@ -58,5 +119,6 @@ const deleteBlogs = async function (req, res) {
   
 
 module.exports.createBlogs = createBlogs
+module.exports.getBlogs=getBlogs
 module.exports.updateBlog = updateBlog
 module.exports.deleteBlogs=deleteBlogs
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,6 +10,7 @@ router.get("/test-me", function (req, res) {
 })
  
 router.post("/authors", authorController.createAuthor)
+router.get("/blog",BlogsController.getBlogs)
 router.post("/Blogs", BlogsController.createBlogs)        
 router.put("/blogs/:blogId", BlogsController.updateBlog)
 router.delete("/blogs/:blogId", BlogsController.deleteBlogs)
